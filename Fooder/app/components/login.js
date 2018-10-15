028b8547260cc8cd2cb13b199a515a4508c121a0
branimircvitanusic <37627437+branimircvitanusic@users.noreply.github.com>
2018-10-15 19:32:10 +0200

    Dodan login al nije povezan

--- Fooder/app/components/login.js
+++ Fooder/app/components/login.js
@@ -1,58 +1,61 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import fire from './config/Fire';
+import Modal from 'react-responsive-modal';
 
 class Login extends Component {
   constructor(props) {
     super(props);
-    this.login = this.login.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.signup = this.signup.bind(this);
-    this.state = {
-      email: '',
-      password: ''
-    };
+
+    this.state = {open : false}
+
   }
 
-  handleChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
-  }
 
-  login(e) {
-    e.preventDefault();
-    fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
-    }).catch((error) => {
-        console.log(error);
-      });
-  }
-
-  signup(e){
-    e.preventDefault();
-    fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
-    }).then((u)=>{console.log(u)})
-    .catch((error) => {
-        console.log(error);
-      })
-  }
-  render() {
-    return (
-       <div className="col-md-6">
-       <form>
-      <div class="form-group">
-       <label for="exampleInputEmail1">Email address</label>
-       <input value={this.state.email} onChange={this.handleChange} type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-       <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-      </div>
-       <div class="form-group">
-      <label for="exampleInputPassword1">Password</label>
-      <input value={this.state.password} onChange={this.handleChange} type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
-      </div>
-      <button type="submit" onClick={this.login} class="btn btn-primary">Login</button>
-      <button onClick={this.signup} style={{marginLeft: '25px'}} className="btn btn-success">Signup</button>
- </form>
+  onOpenModal = () => {
+    this.setState({ open: true });
+  };
  
- </div>
+  onCloseModal = () => {
+    this.setState({ open: false });
+  };
+ 
+  render() {
+    const { open } = this.state;
+    return (
+      <div className="text-center">
+        <button class="btn-outline-primary btn-lg btn-outline-dark" onClick={this.onOpenModal}>Open modal</button>
+        <Modal open={open} onClose={this.onCloseModal}>
+       
+        <div className="modal-fade">
+          <div className="modal-header">
+          <h5 className="modal-title">Logirajte se na svoj račun ili se registrirajte ukoliko ga ne posjedujete</h5>
+          </div>
+          <div className="modal-body">
+          
+          <div class="form-group">
+            <label for="E-mail" class="control-label">Upišite vaš email:</label>
+            <input type="text" class="form-control" id="E mail" value={this.state.inputEmail}></input>
+          </div>
+          
+         <div class="form-group">
+         
+            <label for="password" class="control-label">Upišite vašu lozinku:</label>
+            <input type="password" class="form-control" id="password" value={this.state.inputPassword}></input>
+          </div>
+
+         </div>
+
+         
+         <div className="footer modal-footer">
+                <button type="button" className="btn btn-success">Login</button>
+                <button type="button" className="btn btn-secondary">Register</button>
+         </div>
+      
+        
+          </div>
+        </Modal>
+      </div>
     );
   }
 }
+
 export default Login;
